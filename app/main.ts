5a772136752fdd4ac5dc3db178885b07a4863183
Thorsten Herbst <herbst.thorsten@icloud.com>
2016-08-15 16:26:41 +0200

    bootstrap is running

---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import {platformBrowserDynamic, bootstrap} from '@angular/platform-browser-dynamic'
+import {AppModule} from "./app.module";
+
+
+platformBrowserDynamic().bootstrapModule(AppModule);
